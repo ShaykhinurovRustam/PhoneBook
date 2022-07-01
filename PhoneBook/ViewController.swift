68e929e54f47c4e889459183f0f12d041bfe8c8f
Rustam Shaykhinurov <89419894+ShaykhinurovRustam@users.noreply.github.com>
2022-07-01 13:46:54 +0300

    Initial Commit

---
+++ PhoneBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhoneBook
+//
+//  Created by Рустам Шайхинуров on 01.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
